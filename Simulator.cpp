bb6de232df840c88160e2f179b250d0d86c5fbae
Pioooooo <bowen.sun@qq.com>
2020-07-06 11:27:48 +0800

    init

---
+++ Simulator.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Pioooooo on 2020/7/6.
+//
+
+#include "Simulator.h"
+
+void Simulator::IF()
+{
+
+}
+
+void Simulator::ID()
+{
+
+}
+
+void Simulator::EX()
+{
+
+}
+
+void Simulator::MEM()
+{
+
+}
+
+void Simulator::WB()
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+
+
+
+
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
